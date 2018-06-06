4bc9d250fd3eb5f2a885368d32e340fdfcb9ad70
Chuck Lu <chuck.lu@qq.com>
2018-06-06 16:57:12 +0800

    Covert a data row to an instance of specified class

---
+++ Utility/ExtensionMethods.cs
@@ -0,0 +1,30 @@
+﻿using System.ComponentModel;
+using System.Data;
+
+namespace Utility
+{
+    public static class ExtensionMethods
+    {
+        /// <summary>
+        /// Covert a data row to an instance of specified class
+        /// https://stackoverflow.com/a/9200446
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="row"></param>
+        /// <returns></returns>
+        public static T ToType<T>(this DataRow row) where T : new()
+        {
+            T obj = new T();
+            var props = TypeDescriptor.GetProperties(obj);
+            foreach (PropertyDescriptor prop in props)
+            {
+                if (row.Table.Columns.IndexOf(prop.Name) >= 0
+                    && row[prop.Name].GetType() == prop.PropertyType)
+                {
+                    prop.SetValue(obj, row[prop.Name]);
+                }
+            }
+            return obj;
+        }
+    }
+}
